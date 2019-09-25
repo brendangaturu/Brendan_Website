3f95c01cd5b604c80bad600507967c7a0febe9cf
brendan <brendangaturu@gmail.com>
2019-09-25 13:36:28 +0300

    url paths configured , changes in the seetings.py file

--- brendan_project/settings.py
+++ brendan_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7)+3*klah&=*f3lgghh)u)vo1$-+7lkr@!+ivh*ievarchqypa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'brendan_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
