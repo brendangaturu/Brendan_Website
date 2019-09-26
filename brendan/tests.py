8b4f0721e4573db0d03dee01c01d2c9a2b84ff42
brendan <brendangaturu@gmail.com>
2019-09-26 11:41:56 +0300

    static directory configured, first tests written

--- brendan/tests.py
+++ brendan/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageViewTest(SimpleTestCase):
+
+    def test_home_page_view_status_code(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
