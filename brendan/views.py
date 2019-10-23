4ddc543183be254da1ef4d4b2b73eab39091bf24
brendan <brendangaturu@gmail.com>
2019-10-23 12:53:28 +0300

    services and contact template

--- brendan/views.py
+++ brendan/views.py
@@ -3,3 +3,11 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class ServicesPageView(TemplateView):
+    template_name = 'services.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
