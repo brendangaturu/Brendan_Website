eea34ac20293961c6ca22d7a3ee3bcfefb3d46e5
brendan <brendangaturu@gmail.com>
2019-12-04 00:38:25 +0300

    contact form

--- brendan/forms.py
+++ brendan/forms.py
@@ -5,5 +5,5 @@ from crispy_forms.helper import FormHelper
 class ContactForm(forms.Form):
     """Email contact form"""
     name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Name'}))
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Enter Your Email'}))
     message = forms.CharField(required=True, widget=forms.Textarea())
